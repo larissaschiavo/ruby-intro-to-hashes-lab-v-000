f8fd45a7699b799a4db3517213420ecbb23ed2bb
Larissa S <larissafschiavo@gmail.com>
2018-01-21 01:30:45 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,10 @@ def monopoly_with_third_tier
 	monopoly = {
 
 		railroads: {
-			pieces: 4, 
-			names: {}, 
-			rent_in_dollars: {}}
+			pieces: 4,
+			names: {},
+			rent_in_dollars: {}
+		}
 	}
 end
 
